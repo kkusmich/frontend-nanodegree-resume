9e22f651e07602b48cb0068f9f77542912eb2c11
kayla <kaylakusmich@gmail.com>
2015-05-28 11:54:21 -0400

    adding 4 objects to resumeBuilder
    Adding project images
    Adding bio image
    Adding 4 objects to resumeBuilder.js

---
+++ js/resumeBuilder.js
@@ -0,0 +1,74 @@
+var bio = {
+	"name" : "Kayla Kusmich",
+	"role" : "Front End Developer",
+	"welcomeMessage" : "Hello, nice meeting you!",
+	"contacts" : {
+		"mobile" : "416-567-6985",
+		"email" : "kaylakusmich@gmail.com",
+		"github" : "kkusmich",
+		"location" : "Mississauga"
+	},
+	"skills" : ["project management", "problem-solving", "analytical", "team player", "communication", "customer service"],
+	"bioPic" : "images/kayla_reduced.jpg"
+}
+
+var projects = {
+	"projects" : [
+	{
+		"title" : "Interactive Resume",
+		"Date" : "May 2015",
+		"Description" : "Building online interactive resume",
+		"Images" : ["images/resume.jpg"]
+	},
+	{
+		"title" : "Isotrak Implementation".
+		"Date" : "June 2012 - Dec 2014".
+		"Description" : "Installation and Implementation of GPS Tracking Solution to 350 Tractors and 3000 Trailers".
+		"Images" : ["images/isotrak1_small.jpg", "images/isotrak2_small.jpg"]
+	}]
+}
+
+var work = {
+	"jobs" : [
+	{
+		"Employer" : "Loblaw Companies Ltd",
+		"Title" : "Project Manager",
+		"Location" : "Brampton",
+		"Date" : "Oct 2010 - Feb 2015",
+		"Description" : "Oversee large-scale, top-priority technology initiatives and custom software development projects with up to $7.5M in budget"
+	},
+	{
+		"Employer" : "Second CUp",
+		"Title" : "Barista",
+		"Location" : "Toronto",
+		"Date" : "Jan 2011 - Dec 2013"
+		"Description" : "Deliver friendly, efficient customer service and to create a warm and welcoming atmosphere for customer through preparing and serving hot and cold beverages and food items."
+	}]
+}
+
+var education = {
+	"schools" : [
+	{
+		"Name" : "York University",
+		"Location" : "Toronto",
+		"Degree" : "Bachelor of Administrative Studies",
+		"Majors" : ["Finance", "Administrative", "Project Management"],
+		"Date" : "Jan 2007 - Aug 2010",
+		"URL" : "www.yorku.ca"
+	},
+	{
+		"Name" : "Bronte College of Canada",
+		"Location" : "Mississauga",
+		"Degree" : "Ontario Secondary Schools of Diploma",
+		"Majors" : ["Business"],
+		"Date" : "Apr 2006 - Dec 2006",
+		"URL" : "www.brontecollege.ca"
+	}],
+	"onlineCourses" : [
+	{
+		"Title" : "Front End Developer",
+		"School" : "Udacity",
+		"Date" : "May 2015",
+		"URL" : "www.udacity.com"
+	}]
+}
