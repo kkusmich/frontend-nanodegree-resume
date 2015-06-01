46541573293ca797b3e4da9d84f17238fb7574e7
kayla <kaylakusmich@gmail.com>
2015-06-01 12:57:09 -0400

    Completing project
    *modify css
    *adding map
    *display all objects

--- js/helper.js
+++ js/helper.js
@@ -44,14 +44,14 @@ var HTMLprojectImage = '<img src="%data%">';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
 var HTMLschoolName = '<a href="#">%data%';
-var HTMLschoolDegree = ' -- %data%</a>';
+var HTMLschoolDegree = '  -- %data%</a>';
 var HTMLschoolDates = '<div class="date-text">%data%</div>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
 var HTMLschoolMajor = '<em><br>Major: %data%</em>';
 
 var HTMLonlineClasses = '<h3>Online Classes</h3>';
 var HTMLonlineTitle = '<a href="#">%data%';
-var HTMLonlineSchool = ' - %data%</a>';
+var HTMLonlineSchool = '  - %data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
 var HTMLonlineURL = '<br><a href="#">%data%</a>';
 
@@ -86,6 +86,10 @@ function logClicks(x,y) {
 
 $(document).click(function(loc) {
   // your code goes here!
+  var x = loc.pageX;
+  var y = loc.pageY;
+
+  logClicks(x,y)
 });
 
 
@@ -129,13 +133,13 @@ function initializeMap() {
     // iterates through school locations and appends each location to
     // the locations array
     for (var school in education.schools) {
-      locations.push(education.schools[school].location);
+      locations.push(education.schools[school].Location);
     }
 
     // iterates through work locations and appends each location to
     // the locations array
     for (var job in work.jobs) {
-      locations.push(work.jobs[job].location);
+      locations.push(work.jobs[job].Location);
     }
 
     return locations;
@@ -171,6 +175,7 @@ function initializeMap() {
     // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
       // your code goes here!
+      infoWindow.open(map, marker);
     });
 
     // this is where the pin actually gets added to the map.
@@ -233,11 +238,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   // Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+map.fitBounds(mapBounds);
+});
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -24,7 +24,7 @@ var projects = {
 		"title" : "Isotrak Implementation",
 		"Date" : "June 2012 - Dec 2014",
 		"Description" : "Installation and Implementation of GPS Tracking Solution to 350 Tractors and 3000 Trailers",
-		"Images" : ["images/isotrak1_small.jpg", "images/isotrak2_small.jpg"]
+		"Images" : ["images/isotrak1small.jpg", "images/isotrak2small.jpg"]
 	}]
 }
 
@@ -74,6 +74,7 @@ var education = {
 }
 
 //header//
+bio.display = function() {
 var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
 $("#header").prepend(formattedRole);
 var formattedName = HTMLheaderName.replace("%data%", bio.name);
@@ -94,6 +95,9 @@ var formattedbioPic = HTMLbioPic.replace("%data%", bio.bioPic);
 $("#header").append(formattedbioPic);
 var formattedwelcome = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
 $("#header").append(formattedwelcome);
+}
+
+bio.display ();
 
 //skills//
 if (bio.skills.length > 0) {
@@ -132,3 +136,83 @@ for (job in work.jobs) {
 }
 
 displayWork ();
+
+function inName(name) {
+	name = bio.name.trim().split(" ");
+	console.log (name);
+	name[1] = name[1].toUpperCase ();
+	name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
+
+	return name[0] + " "+name[1];
+}
+
+$('#main').append(internationalizeButton);
+
+projects.display = function () {
+for (project in projects.projects) {
+	$('#projects').append(HTMLprojectStart);
+
+	var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+	$(".project-entry:last").append(formattedProjectTitle);
+	var formattedprojectDates = HTMLprojectDates.replace("%data%", projects.projects[project].Date);
+	$(".project-entry:last").append(formattedprojectDates);
+	var formattedprojectDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].Description);
+	$(".project-entry:last").append(formattedprojectDescription);
+	var formattedprojectImage = HTMLprojectImage.replace("%data%", projects.projects[project].Images[0]);
+	$(".project-entry:last").append(formattedprojectImage);
+	if (projects.projects[project].Images.length > 1) {
+	var formattedprojectImage = HTMLprojectImage.replace("%data%", projects.projects[project].Images[1]);
+	$(".project-entry:last").append(formattedprojectImage);}
+}
+}
+
+projects.display ();
+
+education.display = function () {
+	for (school in education.schools) {
+		$('#education').append(HTMLschoolStart);
+
+		var formattedschoolName = HTMLschoolName.replace("%data%", education.schools[school].Name);
+		$(".education-entry:last").append(formattedschoolName);
+		var formattedschoolDegree = HTMLschoolDegree.replace("%data%", education.schools[school].Degree);
+		$(".education-entry:last").append(formattedschoolDegree);
+		var formattedschoolDates = HTMLschoolDates.replace("%data%", education.schools[school].Date);
+		$(".education-entry:last").append(formattedschoolDates);
+		var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[school].Location);
+		$(".education-entry:last").append(formattedSchoolLocation);
+		var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", education.schools[school].Majors);
+		$(".education-entry:last").append(formattedSchoolMajor);
+	}
+	for (course in education.onlineCourses) {
+		$("#education").append(HTMLschoolStart);
+
+		$(".education-entry:last").append(HTMLonlineClasses);
+		var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[course].Title);
+		$(".education-entry:last").append(formattedOnlineTitle);
+		var formattedonlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[course].School);
+		$(".education-entry:last").append(formattedonlineSchool);
+		var formattedonlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[course].Date);
+		$(".education-entry:last").append(formattedonlineDates);
+		var formattedonlineURL = HTMLonlineURL.replace("%data%", education.onlineCourses[course].URL);
+		$(".education-entry:last").append(formattedonlineURL);
+	}
+}
+
+education.display ();
+
+$("#mapDiv").append(googleMap);
+
+//footercontacts//
+function displayFooter () {
+var formattedmobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+$("#footerContacts").append(formattedmobile);
+var formattedemail = HTMLemail.replace("%data%", bio.contacts.email);
+$("#footerContacts").append(formattedemail);
+var formattedgithub = HTMLgithub.replace("%data%", bio.contacts.github);
+$("#footerContacts").append(formattedgithub);
+var formattedlocation = HTMLlocation.replace("%data%", bio.contacts.location);
+$("#footerContacts").append(formattedlocation);
+}
+
+displayFooter ();
+
